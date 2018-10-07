2b309af8d5501f0f88cc4a6a717d96366c5592fd
jamiejamiebobamie <jmccrory@vt.edu>
2018-10-07 15:50:54 -0700

    admin can edit and update pledges, but cannot login as admin yet to do so

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
 const mongoose = require('mongoose');
+const adminPassword = "password";
+var admin = false;
 
 var exphbs = require('express-handlebars');
 
@@ -14,6 +17,9 @@ mongoose.connect('mongodb://localhost/rotten-potatoes');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// override with POST having ?_method=DELETE or ?_method=PUT
+app.use(methodOverride('_method'))
+
 // OUR MOCK ARRAY OF PROJECTS
 //let pledges = [
 ////  { name: "Mr. Eric Federberg", amount: "1,000,000" },
@@ -25,10 +31,10 @@ const Pledge = mongoose.model('Pledge', {
   amount: Number
 });
 
-//// INDEX
-//app.get('/', (req, res) => {
-//  res.render('pledges-index', { pledges: pledges });
-//})
+const Password = mongoose.model('Password', {
+    password: String
+});
+
 
 // INDEX
 app.get('/', (req, res) => {
@@ -57,6 +63,84 @@ app.post('/pledges', (req, res) => {
     console.log(err.message);
   })
 })
+
+// SHOW
+app.get('/pledges/:id', (req, res) => {
+  Pledge.findById(req.params.id).then((pledge) => {
+    if (admin == false) {
+    res.render('pledges-show', { pledge: pledge })
+} else {
+    res.render('pledges-show-admin', { pledge: pledge })
+}
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+
+
+//----ADMIN FEATURES -----
+// EDIT Pledge
+app.get('/pledges/:id/edit', (req, res) => {
+  Pledge.findById(req.params.id, function(err, pledge) {
+    res.render('pledges-edit', {pledge: pledge});
+  })
+})
+
+// UPDATE Pledge
+app.put('/pledges/:id', (req, res) => {
+  Pledge.findByIdAndUpdate(req.params.id, req.body)
+    .then(review => {
+      res.redirect(`/pledges/${pledge._id}`)
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+
+//Admin login page route
+app.get('/login', (req, res) => {
+  res.render('admin-login', {});
+//  if (admin == false) {
+//  admin = true;
+//} else {
+//    admin = false;
+//};
+});
+
+// CREATE login password to change admin status to true
+app.post('/', (req, res) => {
+  Password.create(req.body)
+  .then((password) => {
+    if (password == adminPassword) {
+        admin = true;
+      } else {
+          admin = false;
+      };
+    console.log(password);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+});
+});
+
+//function change() {
+//    if (admin == false) {
+//    admin = true;
+//  } else {
+//      admin = false;
+//};
+
+//function change() {
+//  var change = document.getElementById("check");
+//  if (change.value == "false") {
+//    document.test.elements["savereport"].value = "True";
+//    //document.test.submit();
+//  } else {
+//    change.value = "true";
+//  }
+//}
+
 
 app.listen(7000, () => {
   console.log('App listening on port 7000!')
