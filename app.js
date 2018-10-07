3ba8b4e61de5209b004a7ab9e9e4d5e35c4d5b66
jamiejamiebobamie <jmccrory@vt.edu>
2018-10-07 14:09:11 -0700

    Users can see allpledges

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// OUR MOCK ARRAY OF PROJECTS
+let pledges = [
+  { name: "Mr. Eric Federberg", amount: "1,000,000" },
+  { name: "Mr. and Mrs. Eren and Emily Cross", amount: "2,000" }
+]
+
+// INDEX
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Handlebars are Cool!' });
+  res.render('pledges-index', { pledges: pledges });
 })
 
 app.listen(7000, () => {
