35a02215e6faf8d7dcd7a7aaca0e6b1d9b9ea31d
jamiejamiebobamie <jmccrory@vt.edu>
2018-10-08 15:21:53 -0700

    finished?

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const methodOverride = require('method-override')
 const app = express()
 const mongoose = require('mongoose');
 var admin = false;
-const Schema = mongoose.Schema
 
-const port = process.env.PORT || 7000;
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+const bodyParser = require('body-parser');
 
 const exphbs = require('express-handlebars');
 
@@ -14,17 +14,17 @@ const thank_yous = require('./controllers/thank_yous');
 const Pledge = require('./models/pledge');
 const Thankyou = require('./models/thank_you');
 
-// INITIALIZE BODY-PARSER AND ADD IT TO APP
-const bodyParser = require('body-parser');
+const port = process.env.PORT || 7000;
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// override with POST having ?_method=DELETE or ?_method=PUT
+app.use(methodOverride('_method'))
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
-// override with POST having ?_method=DELETE or ?_method=PUT
-app.use(methodOverride('_method'))
 
 app.use('/', pledges);
 app.use( thank_yous);
